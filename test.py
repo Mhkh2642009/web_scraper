4209398643c7bf00a3f609c4d5bfbfbcd14cdc91
Mohamed Hossam <mh2642009@gmail.com>
2026-08-16 21:14:02 +0300

    testing some algorithems

--- test.py
+++ test.py
@@ -1,15 +1,34 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 url = input('enter the url: ')
+wanted = input('what you want: ').split(' ')
 
-response = requests.get(url)
+options = Options()
+options.add_argument("--headless")
 
-if response.status_code == 200:
-    scrap = BeautifulSoup(response.content, 'html.parser')
+driver = webdriver.Chrome(options=options)
 
-    for tag in scrap(["script", "style", "noscript"]):
-        tag.decompose()
+driver.get(url)
 
-    text = scrap.get_text(" ", strip=True)
-    print(text)
+html = driver.page_source
+
+driver.quit()
+
+
+scrap = BeautifulSoup(html, 'html.parser')
+
+# for i in wanted:
+#     block = scrap.find(string=lambda text: text and (i or i.lower() or i.upper()) in text)
+#     parent_block = block.parent
+#     print('block is')
+#     print(parent_block)
+
+
+for tag in scrap(["script", "style", "noscript"]):
+    tag.decompose()
+
+text = scrap.get_text(" ", strip=True)
+print('the whole')
+print(text)
